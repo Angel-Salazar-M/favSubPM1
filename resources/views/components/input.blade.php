d80eba8f6d705163f5804702e7f8b9ba3159a3cc
Angel-Salazar-M <145618356+Angel-Salazar-M@users.noreply.github.com>
2024-03-15 14:45:37 -0600

    First commit

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,8 @@
+@props(['titulo', 'type', 'nombre'])
+
+<label class="pr-2">{{$titulo}}</label>
+<input type={{ $type }} name={{ $nombre }} class="border-2 border-black rounded-lg px-5 py-3 mt-2 mb-2">
+
+@error( $nombre )
+<p>{{ $message }}</p>
+@enderror
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('signup');
+});
+
+Route::Post('/login', [UserController::class, 'login']);
+
+Route::Post('/register', [UserController::class, 'create']);
+
+
+
+
+
