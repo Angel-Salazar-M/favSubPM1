5414c4ae6593b5f07d16830e6b3eb4a7e6072848
Angel-Salazar-M <angel.salazar2025@poma.superate.org.sv>
2024-03-19 16:26:17 -0600

    Angel Salazar

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,8 +1,9 @@
 @props(['titulo', 'type', 'nombre'])
+<div class="">
+    <label class="pr-2">{{$titulo}}</label>
+    <input type={{ $type }} name={{ $nombre }} class="border-2 border-black rounded-lg px-5 py-3 mt-2 mb-2">
 
-<label class="pr-2">{{$titulo}}</label>
-<input type={{ $type }} name={{ $nombre }} class="border-2 border-black rounded-lg px-5 py-3 mt-2 mb-2">
-
-@error( $nombre )
-<p>{{ $message }}</p>
-@enderror
+    @error( $nombre )
+    <p>{{ $message }}</p>
+    @enderror
+</div>
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -4,18 +4,10 @@
     <h1 class="text text-5xl text-center uppercase font-bold">Sign up</h1>
     <form action="/register" method="POST" class="flex flex-col justify-center my-12">
         @csrf
-<div>
     <x-input titulo="Name" type="text" nombre="name"/>
-</div>
-<div>
     <x-input titulo="Email" type="text" nombre="Email"/>
-</div>
-<div>
     <x-input titulo="Password" type="password" nombre="password"/>
-</div>
-<div>
     <x-input titulo="Birthday" type="date" nombre="birthday"/>
-</div>
 <button type="submit" class="border-2 border-black rounded-lg bg bg-blue-200 font-semibold px-5 py-2 mt-8 ">Submit</button>
     </form>
 </div>
